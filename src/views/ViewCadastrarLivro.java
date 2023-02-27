2162c57a948bc98a99bb5cbd6ce1c3c72c3a7e63
Richard <richard.esclima@gmail.com>
2023-02-27 17:08:58 -0300

    implementa view de cadastro de livro

--- src/views/ViewCadastrarLivro.java
+++ src/views/ViewCadastrarLivro.java
@@ -9,9 +9,23 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
 import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ViewCadastrarLivro extends JInternalFrame {
 	private JTextField txtAa;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JLabel lblEditora;
+	private JLabel lblDataDaPublicao;
+	private JLabel lblNmeroDeExemplares;
+	private JLabel lblTtulo;
+	private JLabel lblCdigoIsbn;
+	private JButton btnNewButton_1;
 
 	/**
 	 * Launch the application.
@@ -39,16 +53,95 @@ public class ViewCadastrarLivro extends JInternalFrame {
 		setBounds(0, 0, screenSize.width, screenSize.height);
 		getContentPane().setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Cadastrar Livro");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 60));
-		lblNewLabel.setBounds(546, 199, 537, 105);
-		getContentPane().add(lblNewLabel);
-		
 		txtAa = new JTextField();
 		txtAa.setToolTipText("Nome do Livro");
-		txtAa.setBounds(497, 432, 468, 51);
+		txtAa.setBounds(134, 194, 468, 51);
 		getContentPane().add(txtAa);
 		txtAa.setColumns(10);
+		
+		textField = new JTextField();
+		textField.setToolTipText("Nome do Livro");
+		textField.setColumns(10);
+		textField.setBounds(134, 339, 468, 51);
+		getContentPane().add(textField);
+		
+		textField_1 = new JTextField();
+		textField_1.setToolTipText("Nome do Livro");
+		textField_1.setColumns(10);
+		textField_1.setBounds(134, 489, 468, 51);
+		getContentPane().add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setToolTipText("Nome do Livro");
+		textField_2.setColumns(10);
+		textField_2.setBounds(134, 638, 468, 51);
+		getContentPane().add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setToolTipText("Nome do Livro");
+		textField_3.setColumns(10);
+		textField_3.setBounds(969, 204, 468, 51);
+		getContentPane().add(textField_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setToolTipText("Nome do Livro");
+		textField_4.setColumns(10);
+		textField_4.setBounds(969, 339, 468, 51);
+		getContentPane().add(textField_4);
+		
+		JLabel lblNewLabel = new JLabel("Autor");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		lblNewLabel.setBounds(144, 105, 208, 79);
+		getContentPane().add(lblNewLabel);
+		
+		lblEditora = new JLabel("Editora");
+		lblEditora.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		lblEditora.setBounds(144, 250, 208, 79);
+		getContentPane().add(lblEditora);
+		
+		lblDataDaPublicao = new JLabel("Data da Publicação");
+		lblDataDaPublicao.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		lblDataDaPublicao.setBounds(144, 400, 413, 79);
+		getContentPane().add(lblDataDaPublicao);
+		
+		lblNmeroDeExemplares = new JLabel("Número de Exemplares");
+		lblNmeroDeExemplares.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		lblNmeroDeExemplares.setBounds(144, 550, 447, 79);
+		getContentPane().add(lblNmeroDeExemplares);
+		
+		lblTtulo = new JLabel("Título");
+		lblTtulo.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		lblTtulo.setBounds(983, 115, 208, 79);
+		getContentPane().add(lblTtulo);
+		
+		lblCdigoIsbn = new JLabel("Código ISBN");
+		lblCdigoIsbn.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		lblCdigoIsbn.setBounds(979, 265, 314, 79);
+		getContentPane().add(lblCdigoIsbn);
+		
+		JButton btnNewButton = new JButton("Cadastrar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ViewModalConfirmacao modalConfirmacao = new ViewModalConfirmacao();
+
+				modalConfirmacao.setVisible(true);
+				
+			}
+		});
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		btnNewButton.setBackground(new Color(156, 102, 68));
+		btnNewButton.setBounds(231, 751, 284, 44);
+		getContentPane().add(btnNewButton);
+		
+		btnNewButton_1 = new JButton("Voltar");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		btnNewButton_1.setBounds(134, 66, 85, 21);
+		getContentPane().add(btnNewButton_1);
 
 	}
 }
---
+++ src/views/ViewModalConfirmacao.java
@@ -0,0 +1,87 @@
+package views;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ViewModalConfirmacao extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			ViewModalConfirmacao dialog = new ViewModalConfirmacao();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public ViewModalConfirmacao() {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		// obtém a largura e altura da tela
+		int screenWidth = screenSize.width;
+		int screenHeight = screenSize.height;
+
+		// define a largura e altura da janela
+		int frameWidth = 450;
+		int frameHeight = 300;
+
+		// calcula a posição x e y da janela para centralizá-la na tela
+		int frameX = (screenWidth - frameWidth) / 2;
+		int frameY = (screenHeight - frameHeight) / 2;
+
+		// define a posição e dimensão da janela
+		setBounds(frameX, frameY, frameWidth, frameHeight);
+		
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBackground(new Color(156, 102, 68));
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JLabel lblNewLabel = new JLabel("Tem Certeza?");
+			lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
+			lblNewLabel.setForeground(Color.WHITE);
+			lblNewLabel.setBounds(134, 34, 170, 64);
+			contentPanel.add(lblNewLabel);
+		}
+		{
+			JButton btnNewButton = new JButton("Sim");
+			btnNewButton.setForeground(Color.WHITE);
+			btnNewButton.setBackground(new Color(96, 108, 58));
+			btnNewButton.setBounds(101, 189, 85, 21);
+			contentPanel.add(btnNewButton);
+		}
+		{
+			JButton btnNo = new JButton("Não");
+			btnNo.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					dispose();
+				}
+			});
+			btnNo.setBounds(259, 189, 85, 21);
+			contentPanel.add(btnNo);
+		}
+	}
+
+}
