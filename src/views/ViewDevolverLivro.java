5c161e2f40b8807b6f72cd67bfe41345e146d99d
pauloh-fm <phfarmar@gmail.com>
2023-03-03 01:59:39 -0300

    Change - ViewDevolverLivros

--- src/views/ViewDevolverLivro.java
+++ src/views/ViewDevolverLivro.java
@@ -16,11 +16,11 @@ public class ViewDevolverLivro extends JInternalFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args, String cpf) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					ViewDevolverLivro frame = new ViewDevolverLivro();
+					ViewDevolverLivro frame = new ViewDevolverLivro(cpf);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -31,43 +31,46 @@ public class ViewDevolverLivro extends JInternalFrame {
 
 	/**
 	 * Create the frame.
+	 * @param cpf 
 	 */
-	public ViewDevolverLivro() {
+	public ViewDevolverLivro(String cpf) {
+		
+		
 		getContentPane().setBackground(new Color(221, 161, 94));
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		setClosable(true);
 		setBounds(0, 0, screenSize.width, screenSize.height);
 		getContentPane().setLayout(null);
-		// 
-//		ViewModalBusca modalBusca = new ViewModalBusca("CPF do Usuário");
-//		modalBusca.setVisible(true);
-		// Botão Buscar - Irá fazer a query pelo cpf do usuário - o retorno deve ser uma nova pagina com a listas de livros alugados
-		JButton btnNewButton = new JButton("Buscar");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-//				ViewModalBusca modalBusca = new ViewModalBusca("Insira o CPF do usuário");
 
-//				modalBusca.setVisible(true);
-				
-			}
-		});
-		// Botão 0 - Buscar
-		btnNewButton.setForeground(Color.WHITE);
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		btnNewButton.setBackground(new Color(156, 102, 68));
-		btnNewButton.setBounds(570, 707, 284, 44);
-		getContentPane().add(btnNewButton);
+//		 Botão Buscar - Irá fazer a query pelo cpf do usuário - o retorno deve ser uma nova pagina com a listas de livros alugados
+//		JButton btnNewButton = new JButton("Teste");
+//		btnNewButton.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+////				ViewModalBusca modalBusca = new ViewModalBusca("CPF do Usuário");
+////				modalBusca.setVisible(true);
+//			}
+//		});
+//
+//		// Botão 0 - Buscar
+//		btnNewButton.setForeground(Color.WHITE);
+//		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 25));
+//		btnNewButton.setBackground(new Color(156, 102, 68));
+//		btnNewButton.setBounds(570, 707, 284, 44);
+//		getContentPane().add(btnNewButton);
 		
-		
+
+	ViewListaExemplaresAlugados listaExemplaresAlugados = new ViewListaExemplaresAlugados(cpf);
+	listaExemplaresAlugados.setVisible(true);
+
 		// Botão voltar - 
-		btnNewButton_2 = new JButton("Voltar");
-		btnNewButton_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-			}
-		});
-		btnNewButton_2.setBounds(26, 36, 85, 21);
-		getContentPane().add(btnNewButton_2);
+//		btnNewButton_2 = new JButton("Voltar");
+//		btnNewButton_2.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//				dispose();
+//			}
+//		});
+//		btnNewButton_2.setBounds(26, 36, 85, 21);
+//		getContentPane().add(btnNewButton_2);
 
 	}
 
