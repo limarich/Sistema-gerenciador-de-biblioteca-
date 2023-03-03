9428fa57806be0ac9358308c116e551fc48dc3f4
carloslamark <lamark.barros@gmail.com>
2023-03-03 04:26:44 -0300

    cria editar livro controller

--- src/controllers/EditarLivroController.java
+++ src/controllers/EditarLivroController.java
@@ -1,5 +1,27 @@
 package controllers;
 
-public class EditarLivroController {
+import java.util.Random;
 
+import models.Livro;
+
+public class EditarLivroController extends Livro 
+{
+	Livro livro = new Livro();
+	
+	// codigo, autor, editora, dataPublicacao, titulo
+	public void pegarDados(
+							String codigo, 
+							String autor, 
+							String editora, 
+							String dataPubli, 
+							String titulo,
+							String numExemp
+						 ) 
+	{
+		//pesquisa livro utilizando seu codigo
+		this.livro.setAutor(autor);
+		this.livro.setEditora(editora);
+		this.livro.setDataPublicacao(dataPubli);
+		this.livro.setTitulo(titulo);	
+	}
 }
